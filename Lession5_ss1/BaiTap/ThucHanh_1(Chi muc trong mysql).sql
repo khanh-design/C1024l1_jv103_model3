2e83429b0811cca496cdc758506ca2a65dfa4b6d
khanh-desgin <buiduykhanh0162003@gmail.com>
2025-03-26 21:18:24 +0700

    first commit

--- Lession5_ss1/BaiTap/ThucHanh_1(Chi muc trong mysql).sql
+++ Lession5_ss1/BaiTap/ThucHanh_1(Chi muc trong mysql).sql
@@ -1,3 +1,10 @@
 use `classicmodels`;
 
 select * from Customers;
+
+ALTER TABLE customers ADD INDEX idx_customerName(customerName);
+explain select * from customers where customerName = 'Land Of Toys Inc.';
+
+EXPLAIN SELECT * FROM customers WHERE contactFirstName = 'Jean' or contactFirstName = 'King';
+ALTER TABLE customers ADD INDEX idx_full_name(contactFirstName, contactLastName);
+alter table customers drop index idx_full_name;
