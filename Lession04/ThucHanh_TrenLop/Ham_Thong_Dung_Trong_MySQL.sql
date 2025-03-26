34b0ab8f1c0af1e3e12897a7cc99dd1c4482598b
khanh-desgin <buiduykhanh0162003@gmail.com>
2025-03-26 11:23:29 +0700

    first commit

--- Lession04/ThucHanh_TrenLop/Ham_Thong_Dung_Trong_MySQL.sql
+++ Lession04/ThucHanh_TrenLop/Ham_Thong_Dung_Trong_MySQL.sql
@@ -1,2 +1,15 @@
 use `quanlysinhvien`;
 
+select * from Subject;
+
+-- Tinh tong sum()
+select sum(credit) from subject;
+
+-- Tinh trung binh
+select avg(credit) from subject;
+
+-- ham lay do dai 
+select length(subname) from subject;
+
+-- now() tra ve thoi gio hien tai
+select now();
